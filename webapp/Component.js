f2cd8ec3ba6ece9b7c5de83210e7ff473a07f733
62668607 <asia.ryfka@gmail.com>
2020-04-08 21:35:17 +0200

    Step 36: Device Adaptation

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,10 +1,11 @@
 sap.ui.define([
    "sap/ui/core/UIComponent",
    "sap/ui/model/json/JSONModel",
-   "./controller/HelloDialog"
+   "./controller/HelloDialog",
+   "sap/ui/Device"
 
 
-], function (UIComponent, JSONModel, HelloDialog) {
+], function (UIComponent, JSONModel, HelloDialog, Device) {
    "use strict";
    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
 
@@ -23,6 +24,11 @@ sap.ui.define([
          };
          var oModel = new JSONModel(oData);
          this.setModel(oModel);
+
+         // set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
 
          // set dialog
          this._helloDialog = new HelloDialog(this.getRootControl());
